b3fd7bbc3c51c63d5c05584402c8bf8e8f796b3d
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-06-06 12:08:27 +0530

    woah

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
+const app = express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')

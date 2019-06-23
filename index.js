6afbdc31c7a4efe2ca06ec54bb3c9054ddc9b60f
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-06-24 00:27:34 +0530

    bodyParsing and stuff

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 //Middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 // ROUTES
 
@@ -16,12 +19,19 @@ app.get('/', (req,res) =>{
 });
 
 app.post('/chat', (req,res) => {
-  console.log(req.body);
-  const input = req.body.text;
+
+  // Get the input 
+
+  const input = req.body.text + ' -from the other side, bro';
+
+  // Do any calculations, with the dialogflow or whatever just hardcode
+  // Like I did with the -from the other side, bro
+
+  // Get the response ready
   const response = {
     'output': input,
   };
-
+  // Send the response
   res.send(response);
 });
 

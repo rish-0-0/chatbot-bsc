3862312dc2ed525592238b04c3082ae77fe1ef45
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-06-23 15:47:03 +0530

    please work from our react app

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// ROUTES
+
+app.get('/', (req,res) =>{
+  res.send('Why hello there');
+});
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

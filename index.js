5c7a062b81166beb7a42b80eb49a396244c65e91
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-06-25 10:59:19 +0530

    stack-fix cors

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express();
 
 //Middleware
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 

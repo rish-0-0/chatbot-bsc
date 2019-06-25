420e767b1a9ff782cbd48dbaf1f8acbcf4d41287
Rishabh <f20170111@goa.bits-pilani.ac.in>
2019-06-25 11:06:15 +0530

    forcing cors

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //Middleware
-app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
